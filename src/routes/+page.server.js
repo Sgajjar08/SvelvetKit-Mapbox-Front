33973eb5e870a4de7734704c80d36c51c1ccea44
Sgajjar <s.gajjar@kayrros.com>
2023-05-24 18:29:01 +0200

    testing

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,7 +3,7 @@ export async function load({ fetch }) {
 	try {
 		const response = await fetch('ile-de-france.geojson');
 		const geojsonData = await response.json();
-		console.log(geojsonData);
+
 		// @ts-ignore
 		const bridgesGeoJson = {
 			type: 'FeatureCollection',
